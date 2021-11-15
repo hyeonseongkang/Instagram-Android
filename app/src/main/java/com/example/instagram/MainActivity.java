4f529b8ad9ae92555ad8a2daa72803897c74fa65
hyeonseongkang <gt9697@naver.com>
2021-11-15 21:46:09 +0900

    chore : home logo

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
+
+    private AppCompatButton plusButton, heartButton, chatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plusButton = (AppCompatButton) findViewById(R.id.plusButton);
+        heartButton = (AppCompatButton) findViewById(R.id.heartButton);
+        chatButton = (AppCompatButton) findViewById(R.id.chatButton);
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "plusButton");
+            }
+        });
+
+        heartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "heatButton");
+            }
+        });
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "chatButton");
+            }
+        });
     }
 }
